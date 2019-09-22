322097a5b289043ee233caa5b55723789f4a4d0a
omaiyea <omaiyea@gmail.com>
2019-09-22 14:45:23 -0700

    start option validation

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,24 +2,47 @@
 
 using namespace std;
 
-void welcomeMessage(string *menu, int size);
+void welcomeMessage();
+int getOptions();
+
+//global vars for menu 
+int menuSize = 3;
+string menuOptions[3] = {"Assign seats", "Change seats", "View seats"};
 
 int main()
 {
-    int menuSize = 2;
-    string menuOptions[2] = {"Assign seats", "Change seats"};
+    int option;
 
-    welcomeMessage(menuOptions, menuSize);
+    welcomeMessage();
+    option = getOptions();
 
     return 0;
 }
 
-void welcomeMessage(string *menu, int size)
+//display welcome message and options to user
+void welcomeMessage()
 {
     cout << "\n\n\t\tSEAT ASSIGNMENT";
     cout << "\n\n\nMenu Options:\n";
-    for(int i = 0; i < size; i++){
-        cout << i+1 << ". " << menu[i] << "\n";
+    for(int i = 0; i < menuSize; i++){
+        cout << i+1 << ". " << menuOptions[i] << "\n";
     }
-    cout << "\n\nPlease choose an option: ";
+    cout << "\n";
+}
+
+//get users selection for which action to do
+int getOption(){
+    int option;
+    
+    cout << "\nPlease choose an option by entering its number: ";
+    cin >> option;
+
+    //validation, need to verify is number too
+    while(option > menuSize || option < 0) 
+    {
+        cout << "\nPlease choose a valid option: ";
+        cin >> option;
+    };
+
+    return option;
 }
